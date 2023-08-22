78a6b460d36949fb412dc713d91756758b02c0d1
TechDanil <danilkomarov243242@gmail.com>
2023-08-22 22:30:08 +0500

    created function openDB in db.ts

---
+++ src/settings/settings.ts
@@ -0,0 +1,11 @@
+const DB_NAME = "authDB"
+const ACTUAL_DB_VERSION = 1;
+const OBJECT_STORE_NAME = 'users';
+const KEY_PATH_NAME = "username";
+
+export {
+    DB_NAME,
+    ACTUAL_DB_VERSION,
+    OBJECT_STORE_NAME,
+    KEY_PATH_NAME,
+};
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,25 @@
-interface IDBDatabase {
-    
-}
+import * as dbSettings from '../settings/settings';
 
 const openDB = () => {
     return new Promise((resolve, reject) => {
-        
+        const request = window.indexedDB.open(dbSettings.DB_NAME, dbSettings.ACTUAL_DB_VERSION);
+
+        request.onupgradeneeded = (event) => {
+            const db = (event.target as IDBRequest<IDBDatabase>).result;
+            db.createObjectStore(dbSettings.OBJECT_STORE_NAME, { 
+                keyPath: dbSettings.KEY_PATH_NAME,
+            });
+        }
+
+        request.onsuccess = (event) => {
+            const db = (event.target as IDBRequest<IDBDatabase>).result;
+            resolve(db);
+        }
+
+        request.onerror = (event) => {
+            reject(event);
+        }
     })
 }
+
+export { openDB };
